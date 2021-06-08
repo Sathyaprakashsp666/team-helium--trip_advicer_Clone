d361d6e8b18f8b53594296dd731f8201fd771cd5
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-06-08 13:20:00 +0530

    initail commit

---
+++ main/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Home from './components/LandingPage/Home';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ main/src/components/LandingPage/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* Navbar add below   @ kandur*/}
+
+      {/* middle section add below  @ uday*/}
+
+      {/* bottom section add below @ satya*/}
+    </div>
+  );
+};
+
+export default Home;
