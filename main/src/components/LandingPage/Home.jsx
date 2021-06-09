ceadbbfedc0934d6a0f8bb63606c3dec49d38afd
KANDURI MANOHAR <manoharkanduri@gmail.com>
2021-06-09 11:35:35 +0530

    NavBar

--- main/src/components/LandingPage/Home.jsx
+++ main/src/components/LandingPage/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavBAr } from "./NavBar";
 
 const Home = () => {
   return (
     <div>
       {/* Navbar add below   @ kandur*/}
-
+        <NavBAr/>
       {/* middle section add below  @ uday*/}
 
       {/* bottom section add below @ satya*/}
