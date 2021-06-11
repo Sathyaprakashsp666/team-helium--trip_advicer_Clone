f79509fffb267e9cd37f2378eb1deb1cd23ae702
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-06-11 12:05:12 +0530

    rental page added

--- main/src/components/FetchingRentals/FetchingRental.jsx
+++ main/src/components/FetchingRentals/FetchingRental.jsx
@@ -1,28 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const FetchingRental = () => {
-    return (
-        <div>
-            welcome
-            erg
-            eg
-            5y
+  return <div>
+      
+  </div>;
+};
 
-            5y
-            y5
-            y5
-            y
-            y
-            y
-            y
-            y
-            y
-            y
-            y
-            4y
-            
-        </div>
-    )
-}
-
-export default FetchingRental
+export default FetchingRental;
