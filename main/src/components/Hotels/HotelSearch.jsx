bd14f4fd3986d2dc405759679992a873f041dfa1
uudaykuma <uudaykuma@gmail.com>
2021-06-12 10:57:21 +0530

    hotel serach sorting

--- main/src/components/Hotels/HotelSearch.jsx
+++ main/src/components/Hotels/HotelSearch.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import Header from './Header'
-import LeftSide from './LeftSide'
-import ShowHotelInfo from './ShowHotelInfo'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./Header";
+import LeftSide from "./LeftSide";
+import ShowHotelInfo from "./ShowHotelInfo";
+import SortingDiv from "./SortingDiv";
 
 const HotelSearch = () => {
-    const [hotel, setHotel] = useState('')
-    const [hotelData, setHotelData] = useState([])
+    //   const [hotel, setHotel] = useState("");
+    const [hotelData, setHotelData] = useState([]);
+    // const [page, setPage] = useState(1)
 
-    const handleHotelSearch = async() => {
-        const {data} = await axios.get('http://localhost:4500/pune')
-        setHotelData(data)
-    }
+    const handleHotelSearch = async () => {
+        const { data } = await axios.get(`http://localhost:4500/pune`);
+        setHotelData(data);
+    };
     useEffect(() => {
-       handleHotelSearch()
-    }, [])
-    console.log(hotelData)
+        handleHotelSearch();
+    }, []);
+    //console.log(hotelData);
     return (
         <div>
-        <Header />
-        <LeftSide />
-        
-        {hotelData.map(item => {
-            return <>
-            <ShowHotelInfo  item={item}/>
-            </>
-        })}
-        
+            <Header />
+            <LeftSide />
+            <SortingDiv  hotelData={hotelData}/>
+
+            {hotelData.map((item) => {
+                return (
+                    <>
+                        <ShowHotelInfo item={item} />
+                    </>
+                );
+            })}
+            {/* <button disabled={page === 1} onClick={() => setPage(page - 1)}>Prev Page</button>
+            <button disabled={hotelData.length < 15} onClick={() => setPage(page + 1)}>Next Page</button> */}
         </div>
-    )
-}
+    );
+};
 
-export default HotelSearch
-
+export default HotelSearch;
