78a8e6e810573d48045f65f1156b47be35b11816
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-06-12 10:00:47 +0530

    rental page structure

--- main/src/components/RentalPage/RentalPage.jsx
+++ main/src/components/RentalPage/RentalPage.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+// https://json-mock-server-trip-advicer.herokuapp.com/rentals
+import React from "react";
+import styles from "./RentalPage.module.css";
+import RentalsContainer from "./RentalContainer";
 
 const RentalPage = () => {
-    return (
-        <div>
-            <h1>Rental Page</h1>
-            
+  return (
+    <div>
+      <h1>Rental Page</h1>
+      <div className={styles.rentalas_top_cont}>
+        <div className={styles.rentals_left_cont}></div>
+        <div className={styles.rentals_right_cont}>
+          <RentalsContainer />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RentalPage
+export default RentalPage;
