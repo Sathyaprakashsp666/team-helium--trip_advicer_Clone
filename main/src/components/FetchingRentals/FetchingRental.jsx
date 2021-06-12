1a3419b54eefd77ad0257100f82b1105e6878b69
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-06-12 09:23:12 +0530

    Rental page initial commit

--- main/src/components/FetchingRentals/FetchingRental.jsx
+++ main/src/components/FetchingRentals/FetchingRental.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+// https://json-mock-server-trip-advicer.herokuapp.com/rentals
 
+import React from "react";
+import styles from "./FetchingRentals.module.css";
+import RentalsContainer from "./RentalsContainer";
 const FetchingRental = () => {
-  return <div>
-      
-  </div>;
+  return (
+    <>
+      <h1>Holiday Rentals in Vagamoon</h1>
+      <div className={styles.rentalas_top_cont}>
+        <div className={styles.rentals_left_cont}></div>
+        <div className={styles.rentals_right_cont}>
+          <RentalsContainer/>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FetchingRental;
