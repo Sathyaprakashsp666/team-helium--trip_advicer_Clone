8903838ab9d43bce793c090744c94f3a20badf71
uudaykuma <uudaykuma@gmail.com>
2021-06-12 14:47:16 +0530

    hotel serach sorting

--- main/src/components/Hotels/SortingDiv.jsx
+++ main/src/components/Hotels/SortingDiv.jsx
@@ -1,27 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import ShowHotelInfo from "./ShowHotelInfo";
 
-const SortingDiv = (hoteldata) => {
-    const data = hoteldata.hotelData
-    const [optionValue, setOptionValue] = useState('')
-    //console.log(optionValue)
-    // for (let i = 0; i < data.length; i++) {
-    //     for (let j = 0; j < data.length -i -1; j++) {
-    //     }}
-    
-        
-    return (
-        <div className="sorting-div">
-            <p><span>1,653 properties</span> in Pune</p>
-            <div className="sorting-options">
-                sort by:<select onChange={(e) => setOptionValue(e.target.value)}>
-                    <option value="travel_ranked">Traveller Ranked</option>
-                    <option value="lowToHigh">Price (low to high)</option>
-                    <option value="highToLow">Price (high to low)</option>
-                </select>
-            </div>
+const SortingDiv = ({ hoteldata }) => {
+  //console.log(hoteldata[0])
+//   const defaultValue = [];
+//   defaultValue.push(hoteldata);
+//   console.log(defaultValue);
 
+  const [optionValue, setOptionValue] = useState("");
+  console.log(optionValue)
+
+  const lowToHigh = [];
+  const highToLow = [];
+
+
+
+  hoteldata.sort(function (a, b) {
+    return Number(a.price) - Number(b.price);
+  });
+  lowToHigh.push(hoteldata);
+  console.log(lowToHigh[0]);
+
+
+
+  hoteldata.sort(function (a, b) {
+    return Number(b.price) - Number(a.price);
+  });
+  highToLow.push(hoteldata);
+  console.log(highToLow[0]);
+
+  return (
+    <>
+      <div className="sorting-div">
+        <p>
+          <span>1,653 properties</span> in Pune
+        </p>
+        <div className="sorting-options">
+          sort by:
+          <select onChange={(e) => setOptionValue(e.target.value)}>
+            <option value="travel_ranked">Traveller Ranked</option>
+            <option value="lowToHigh">Price (low to high)</option>
+            <option value="highToLow">Price (high to low)</option>
+          </select>
         </div>
-    )
-}
+      </div>
+      {hoteldata.map((item) => {
+        return (
+          <>
+            <ShowHotelInfo item={item} />
+          </>
+        );
+      })}
+    </>
+  );
+};
 
-export default SortingDiv
+export default SortingDiv;
