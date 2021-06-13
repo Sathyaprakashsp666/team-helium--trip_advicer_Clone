cba6052980981e88d55bab4b885d51d91de678a5
KANDURI MANOHAR <manoharkanduri@gmail.com>
2021-06-13 07:27:35 +0530

    icon sizes adjusted

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './components/LandingPage/Home';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AbhuDhabi } from './components/LandingPage/ThingsTodo/AbhuDhabi';
 function App() {
   return (
     <div>
       <Home/>
+      <AbhuDhabi/>
     </div>
   );
 }
