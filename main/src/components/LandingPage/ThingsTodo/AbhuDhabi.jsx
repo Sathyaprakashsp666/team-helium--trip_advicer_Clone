2e512657bbdd06f056174459dc290153a419de84
KANDURI MANOHAR <manoharkanduri@gmail.com>
2021-06-17 11:49:58 +0530

    TopAttractionsAdded

--- main/src/components/LandingPage/ThingsTodo/AbhuDhabi.jsx
+++ main/src/components/LandingPage/ThingsTodo/AbhuDhabi.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import 'react-multi-carousel/lib/styles.css';
-// import {PopularThings} from './PopularThings';
-// import {WaysToTourDhubai} from './waystotour'
-// import {Cultural} from './culturalandtheme'
-// import Carousel from './TopAttractions'
+import {PopularThings} from './PopularThings';
+import {WaysToTourDhubai} from './waystotour'
+import {Cultural} from './culturalandtheme'
 import {TopAttractions} from './TopAttractions'
 function AbhuDhabi(){
     return (
         <div>
-            {/* <PopularThings/> 
+            <PopularThings/> 
            <WaysToTourDhubai/>
-           <Cultural/> */}
+           <Cultural/>
            <TopAttractions/>
         </div>
     )
